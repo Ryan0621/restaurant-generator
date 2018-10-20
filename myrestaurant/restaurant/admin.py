51b404b0f8596dd09912c290fe89be9ae67dabe2
Ryan0621 <ryan07063@gmail.com>
2018-10-20 16:14:27 -0700

    development

--- myrestaurant/restaurant/admin.py
+++ myrestaurant/restaurant/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Restaurant, OpeningTime, OutletType, Affordability, CuisineType
 
 # Register your models here.
+admin.site.register(Restaurant)
+admin.site.register(OutletType)
+admin.site.register(CuisineType)
+admin.site.register(OpeningTime)
+admin.site.register(Affordability)
--- myrestaurant/restaurant/models.py
+++ myrestaurant/restaurant/models.py
@@ -1,17 +1,59 @@
 from django.db import models
+from address.models import AddressField
 
 # Create your models here.
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+class OpeningTime(models.Model):
+    WEEKDAYS = (
+       (1, 'Monday'),
+       (2, 'Tuesday'),
+       (3, 'Wednesday'),
+       (4, 'Thursday'),
+       (5, 'Friday'),
+       (6, 'Saturday'),
+       (7, 'Sunday'),
+    )
+    weekday = models.IntegerField(choices=WEEKDAYS)
+    from_hour = models.TimeField()
+    to_hour = models.TimeField()
     def __str__(self):
-        return self.question_text
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        return self.WEEKDAYS[self.weekday - 1][1] + " " + str(self.from_hour) + "-" + str(self.to_hour)
 
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+class OutletType(models.Model):
+    # OUTLET_TYPE_CHOICES = (
+    #     ('BR', 'Bar'),
+    #     ('BV', 'Beverage'),
+    #     ('CF', 'Cafe / Coffee Shop'),
+    #     ('DS', 'Dessert'),
+    #     ('FF', 'Fast Food'),
+    #     ('FC', 'Food Court'),
+    #     ('RS', 'Restaurant'),
+    # )
+    outlet_type = models.CharField(max_length=20)
     def __str__(self):
-        return self.choice_text
+        return self.outlet_type
+
+
+class CuisineType(models.Model):
+    cuisine_type = models.CharField(max_length=20)
+    def __str__(self):
+        return self.cuisine_type
+
+class Affordability(models.Model):
+    class Meta:
+        verbose_name_plural = "Affordabilities"
+
+    affordability = models.CharField(max_length=20)
+    def __str__(self):
+        return self.affordability
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=200)
+    address = AddressField(on_delete=models.CASCADE)
+    outlet_type = models.ForeignKey(OutletType, on_delete=models.CASCADE)
+    cuisine_type = models.ForeignKey(CuisineType, on_delete=models.CASCADE)
+    affordability = models.ForeignKey(Affordability, on_delete=models.CASCADE)
+    opening_times = models.ManyToManyField(OpeningTime)
+    vegan_friendly= models.BooleanField()
+    halal=models.BooleanField()
+    def __str__(self):
+        return self.name
