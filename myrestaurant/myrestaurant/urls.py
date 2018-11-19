2a70b6d76b13de55ae4d0724094cd72257bebc24
Ryan0621 <ryan07063@gmail.com>
2018-11-18 16:18:27 -0800

    Convert to class based, add several classes and implements API root relationships

--- myrestaurant/myrestaurant/urls.py
+++ myrestaurant/myrestaurant/urls.py
@@ -1,4 +1,4 @@
-"""myrestaurant URL Configuration 
+"""myrestaurant URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -20,5 +20,5 @@ from django.conf.urls import url
 urlpatterns = [
     url(r'^', include('restaurant.urls')),
     path('admin/', admin.site.urls),
-    url(r'^api-auth/', include('rest_framework.urls'))
+    #url(r'^api-auth/', include('rest_framework.urls'))
 ]
