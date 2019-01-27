f51975fe06b4868118581d1095422e21d1a80fb9
Ryan0621 <ryan07063@gmail.com>
2019-01-27 15:34:07 -0800

    Change url for API

--- myrestaurant/restaurant/urls.py
+++ myrestaurant/restaurant/urls.py
@@ -3,12 +3,12 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    path('', views.api_root),
-    path('restaurant', views.RestaurantList.as_view(), name='restaurant-list',),
-    path('restaurant/<int:pk>/', views.RestaurantDetail.as_view()),
-    path('cuisine_type/', views.CuisineTypeList.as_view(), name='cuisine_type-list',),
-    path('outlet_type/', views.OutletTypeList.as_view(), name='outlet_type-list',),
-    path('affordability/', views.AffordabilityList.as_view(), name='affordability-list',),
+    path('api', views.api_root),
+    path('api/restaurant', views.RestaurantList.as_view(), name='restaurant-list',),
+    path('api/restaurant/<int:pk>/', views.RestaurantDetail.as_view()),
+    path('api/cuisine_type/', views.CuisineTypeList.as_view(), name='cuisine_type-list',),
+    path('api/outlet_type/', views.OutletTypeList.as_view(), name='outlet_type-list',),
+    path('api/affordability/', views.AffordabilityList.as_view(), name='affordability-list',),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
