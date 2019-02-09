f7a7916bcbc57a287f8a624062b5930786a1510d
Ryan0621 <ryan07063@gmail.com>
2019-02-09 15:44:34 -0800

    change static directory

--- myrestaurant/myrestaurant/settings.py
+++ myrestaurant/myrestaurant/settings.py
@@ -133,9 +133,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static-admin/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static-admin/')
 
 GOOGLE_API_KEY = 'AIzaSyD-8FXmzbJljYrb2wfgjOEIY3E9JP3t78M'
 
